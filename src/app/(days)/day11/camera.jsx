a7fa91a6a9d63d65210729a0439d122e7c1357de
levannghia <levannghia.tdc2018@gmail.com>
2024-01-09 16:46:19 +0700

    Len

--- src/app/(days)/day11/camera.jsx
+++ src/app/(days)/day11/camera.jsx
@@ -1,32 +1,46 @@
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
-import React, { useEffect } from 'react'
-import { Stack } from 'expo-router'
-import { useCameraPermission, useCameraDevice } from 'react-native-vision-camera'
+import React, { useEffect, useCallback, useState } from 'react'
+import { Stack, useFocusEffect } from 'expo-router'
+import { useCameraPermission, useCameraDevice, Camera } from 'react-native-vision-camera'
 
 const CameraScreen = () => {
     const device = useCameraDevice('back')
     const { hasPermission, requestPermission } = useCameraPermission()
-    console.log(hasPermission);
+    const [isActive, setIsActive] = useState(false)
+    // const isFocused = useIsFocused()
+    // const appState = useAppState()
+    // const isActive = isFocused && appState === "active"
+
+    useFocusEffect(() => {
+        useCallback(() => {
+            setIsActive(true);
+            return () => {
+                setIsActive(false);
+            }
+        }, [])
+    })
+
     useEffect(() => {
         if (!hasPermission) {
             requestPermission();
         }
     }, [hasPermission])
 
+    console.log(isActive);
+
     if (!hasPermission) {
         return <ActivityIndicator />
     }
 
-    
-  if (device == null) return <Text>Camera device not found</Text>
+    if (device == null) return <Text>Camera device not found</Text>
 
     return (
-        <View>
+        <View style={{ flex: 1 }}>
             <Stack.Screen options={{ headerShown: false }} />
             <Camera
                 // style={StyleSheet.absoluteFill}
                 device={device}
-                isActive={true}
+                isActive={isActive}
             />
         </View>
     )
