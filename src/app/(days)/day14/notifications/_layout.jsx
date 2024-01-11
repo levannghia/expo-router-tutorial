296c4fc1658160e0ee6b3fa57e2072675c0b5c8d
levannghia <levannghia.tdc2018@gmail.com>
2024-01-11 10:54:53 +0700

    Up

---
+++ src/app/(days)/day14/notifications/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, Platform } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router'
+import { AntDesign } from '@expo/vector-icons';
+
+const AppWithNotificationsLayout = () => {
+  return (
+    <Slot/>
+  )
+}
+
+export default AppWithNotificationsLayout
+
+const styles = StyleSheet.create({})
